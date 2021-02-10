e17b7ad72bff40b8f5f1397a2e85adb1e23ba39f
Alastair Ong <alastair.ong@gmail.com>
2021-02-10 13:11:34 +0000

    Update binary cache keys

--- modules/default.nix
+++ modules/default.nix
@@ -15,7 +15,9 @@ in
   ];
 
   nix.binaryCachePublicKeys = [
+    # deprecated
     "cache.holo.host-1:lNXIXtJgS9Iuw4Cu6X0HINLu9sTfcjEntnrgwMQIMcE="
+    "cache.holo.host-2:ZJCkX3AUYZ8soxTLfTb60g+F3MkWD7hkH9y8CgqwhDQ="
   ];
 
   nix.gc = {
